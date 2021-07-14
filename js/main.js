1e23d4fd29470b495b1a9a35feeb15bdcb665431
Pietro <pietrospitella@gmail.com>
2021-07-14 14:12:10 +0200

    pw gen done ✅

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = prompt("inserisci il tuo nome");
+
+var cognome = prompt("inserisci il tuo cognome");
+
+var colore = prompt("inserisci il tuo colore preferito");
+
+document.getElementById("your_pw").innerHTML = "la tua password è " + nome + cognome + colore + "21";
+
